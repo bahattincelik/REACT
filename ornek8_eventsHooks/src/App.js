c1146d5d452a3b545437bacf65a2e8931afb3391
Bahattin Celik <75189249+bahattincelik@users.noreply.github.com>
2021-06-09 16:06:17 +0300

    day06

--- ornek8_eventsHooks/src/App.js
+++ ornek8_eventsHooks/src/App.js
@@ -1,8 +1,15 @@
+import ClassComponent from "./ClassComponent";
 import Events from "./Events";
+import HookOrnek from "./HookOrnek";
+import Hooks from "./Hooks";
 function App() {
   return (
     <div className="App">
       <Events />
+      <ClassComponent sayi="10" />
+      <ClassComponent sayi="100" />
+      <Hooks />
+      <HookOrnek />
     </div>
   );
 }
