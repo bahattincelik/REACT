79b57164ad8f692c9f4853c1def4689bcfcfc721
Bahattin Celik <75189249+bahattincelik@users.noreply.github.com>
2021-06-14 22:00:02 +0300

    react son

--- ornek11_conditions_forms/src/App.js
+++ ornek11_conditions_forms/src/App.js
@@ -1,11 +1,12 @@
 import ConditonalRender from "./ConditonalRender";
 import Form01 from "./Form01";
-
+import Form02 from "./Form02";
 function App() {
   return (
     <div className="App">
       {/* <ConditonalRender /> */}
-      <Form01 />
+      {/* <Form01 /> */}
+      <Form02 />
     </div>
   );
 }
